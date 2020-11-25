0a977f9203dcf312ba05372a2d02c25669ffc28c
alif <alif@admin.com>
2020-11-25 13:17:38 +0600

    trp

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Amplify from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+
+// Amplify.configure(awsconfig);
+
+// function App() {
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <AmplifySignOut />
+//                 <h2>My App Content</h2>
+//             </header>
+//         </div>
+//     );
+// }
+
+// export default withAuthenticator(App);
+
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-
-Amplify.configure(awsconfig);
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <AmplifySignOut />
-                <h2>My App Content</h2>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
 }
 
 export default withAuthenticator(App);
